411f282a19b640252ad46184a3e38473f0d7f999
FalahOmar16 <omarffallahh@gmail.com>
2025-05-15 01:41:23 +0200

    aggiunta funz admin

--- index.php
+++ index.php
@@ -5,11 +5,7 @@ $password = "";
 $database = "mydb"; // your desired database name
 
 // Connect to MySQL server (without selecting a database yet)
-<<<<<<< HEAD
 $conn = mysqli_connect($servername, $username, $password,$database);
-=======
-$conn = mysqli_connect($servername, $username, $password, $database);
->>>>>>> 60bf032 (aggiunta admin commands)
 
 // Check connection
 if (!$conn) {
@@ -33,45 +29,20 @@ if (mysqli_num_rows($result) > 0) {
 }
 // sql to create table
 $sqlA = "CREATE TABLE IF NOT EXISTS ADMINISTRATOR (
-<<<<<<< HEAD
     id VARCHAR(10) PRIMARY KEY,
     psw VARCHAR(30) NOT NULL
 )";
 
 $sqlC = "CREATE TABLE IF NOT EXISTS CLIENTE (
     id VARCHAR(10) PRIMARY KEY,
-=======
-    n INT AUTO_INCREMENT PRIMARY KEY,
-    id VARCHAR(10) NOT NULL,
-    psw VARCHAR(30) NOT NULL
-)";
-
-$sqlM = "CREATE TABLE IF NOT EXISTS MERCE (
-    id VARCHAR(10) PRIMARY KEY,
-    nome VARCHAR(20) NOT NULL, 
-    prezzo DECIMAL(6,2) NOT NULL,
-    qta INT NOT NULL
-)";
-
-$sqlC = "CREATE TABLE IF NOT EXISTS CLIENTE (
-    n INT AUTO_INCREMENT PRIMARY KEY,
-    id VARCHAR(10) NOT NULL UNIQUE,
->>>>>>> 60bf032 (aggiunta admin commands)
     psw VARCHAR(30) NOT NULL
 )";
 
 $sqlF = "CREATE TABLE IF NOT EXISTS FORNITORE (
-<<<<<<< HEAD
     id VARCHAR(10) PRIMARY KEY,
     psw VARCHAR(30) NOT NULL
 )";
 
-=======
-    n INT AUTO_INCREMENT PRIMARY KEY,
-    id VARCHAR(10) NOT NULL UNIQUE,
-    psw VARCHAR(30) NOT NULL
-)";
->>>>>>> 60bf032 (aggiunta admin commands)
 if (mysqli_query($conn, $sqlA)) {
   echo "Table A OK \n";
 } else {
@@ -88,17 +59,7 @@ if (mysqli_query($conn, $sqlC)) {
     echo "Error creating tableF: " . mysqli_error($conn);
   }
 
-<<<<<<< HEAD
   $sqlInsert = "INSERT INTO ADMINISTRATOR (id, psw)
-=======
-   if (mysqli_query($conn, $sqlM)) {
-    echo "Table M OK \n";
-  } else {
-    echo "Error creating tableF: " . mysqli_error($conn);
-  }
-/*
- $sqlInsert = "INSERT INTO ADMINISTRATOR (id, psw)
->>>>>>> 60bf032 (aggiunta admin commands)
     VALUES ('ADMIN', 'ADMIN')";
 
 if ($conn->query($sqlInsert) === TRUE) {
@@ -107,11 +68,7 @@ if ($conn->query($sqlInsert) === TRUE) {
     echo "Error: " . $sqlInsert . "<br>" . $conn->error;
   }
 
-<<<<<<< HEAD
  
-=======
- */
->>>>>>> 60bf032 (aggiunta admin commands)
 // Close connection
 mysqli_close($conn);
 ?>
@@ -131,21 +88,10 @@ mysqli_close($conn);
         <option name="fornitore" value="3">Fornitore</option>
     </select>
 
-<<<<<<< HEAD
     <input type="text" name="id" placeholder="id">
     <input type="text" name="psw" placeholder="psw">
     <input type="submit">
     </form>
     <a href="sign-up.php">Sei un cliente e non hai un account?</a>
 </body>
-=======
-    <input type="text" name="id" placeholder="id" required>
-    <input type="text" name="psw" placeholder="psw" required>
-    <input type="submit">
-    </form>
-    <a href="sign-up.php">Sei un cliente e non hai un account?</a>
-    
-
-  </body>
->>>>>>> 60bf032 (aggiunta admin commands)
 </html>
